e3c598097db5134f89de8ee59f1febf2bf168423
Dinh Quan <quanvonguyendinh@gmail.com>
2025-08-14 01:58:08 +0700

    feat(list): implement for searchAllListsByName

--- microsoft-lists-sql/src/modules/list/list.controller.spec.ts
+++ microsoft-lists-sql/src/modules/list/list.controller.spec.ts
@@ -1,14 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ListController } from './list.controller';
 import { ListService } from './list.service';
+import { List } from '../../entities/list.entity';
+import { ListRepository } from '../../modules/list/list.repository';
 
 describe('ListController', () => {
   let controller: ListController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: 'database.sqlite',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        }),
+        TypeOrmModule.forFeature([List]),
+      ],
       controllers: [ListController],
-      providers: [ListService],
+      providers: [ListService, ListRepository],
     }).compile();
 
     controller = module.get<ListController>(ListController);
@@ -16,5 +28,33 @@ describe('ListController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('searchAllListsByName', () => {
+    it('should return lists if input matches the search term', async () => {
+      const input = 'project';
+      const result = await controller.searchAllListsByName(input);
+
+      expect(result).toBeDefined();
+      expect(result).not.toBeNull();
+      expect(Array.isArray(result)).toBe(true);
+      if (result) {
+        expect(result[0]).toHaveProperty('listId');
+        expect(result[0]).toHaveProperty('icon');
+        expect(result[0]).toHaveProperty('color');
+        expect(result[0]).toHaveProperty('listName');
+        expect(result[0]).toHaveProperty('workspaceName');
+      }
+    });
+
+    it('should return an empty array if input does not match the search term', async () => {
+      const input = 'nonexistent';
+      const result = await controller.searchAllListsByName(input);
+
+      expect(result).toBeDefined();
+      expect(result).not.toBeNull();
+      expect(Array.isArray(result)).toBe(true);
+      expect(result).toEqual([]);
+    });
   });
 });
