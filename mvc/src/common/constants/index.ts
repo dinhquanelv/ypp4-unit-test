d8f4471d2c63038f16401820f839156d256d274f
Dinh Quan <quanvonguyendinh@gmail.com>
2025-08-26 16:43:51 +0700

    feat(controller): implement parameter decorator

--- mvc/src/common/constants/index.ts
+++ mvc/src/common/constants/index.ts
@@ -3,3 +3,4 @@ export const ROUTES_METADATA = 'routesMetadata';
 export const PREFIX_METADATA = 'prefixMetadata';
 export const FINAL_PATH_METADATA = 'finalPathMetadata';
 export const HTTP_CODE_METADATA = 'httpCodeMetadata';
+export const PARAM_METADATA_KEY = 'custom:params';
--- mvc/src/common/types/index.ts
+++ mvc/src/common/types/index.ts
@@ -1,7 +1,21 @@
+import { ParamType } from '../enums';
+
 export type ClassType<T = unknown> = new (...args: unknown[]) => T;
+
+export type ParameterDecorator = (
+  target: object,
+  propertyKey: string | symbol,
+  parameterIndex: number,
+) => void;
 
 export interface RouteMetadata {
   method: string;
   path: string;
   handler: string;
+}
+
+export interface ParamMetadata {
+  type: ParamType;
+  key?: string;
+  index: number;
 }
