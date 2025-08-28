d8cb8bdc68900c0d8ad4a345a95347dd76595978
Dinh Quan <quanvonguyendinh@gmail.com>
2025-08-28 13:16:30 +0700

    fix(app): change lib

--- mvc/src/app.controller.ts
+++ mvc/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from './decorator/controller/controller.decorator';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Get } from './decorator/routes/routes.decorator';
 
